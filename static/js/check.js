65027230a40b9fd08240b8d4784b915ec7074c85
Myst <cadellgriffith@gmail.com>
2020-10-21 17:46:04 +0100

    second commit

---
+++ static/js/check.js
@@ -0,0 +1,45 @@
+// check game object
+const checkgame = {
+    players:{
+        player1:{
+            score:0,
+            numberofcards:0
+        },
+        player2:{
+            score:0,
+            numberofcards:0
+        },
+        play: function(){
+
+        },
+        finish: function(){
+
+        }
+    },
+    gamescore:{
+
+    },
+    messages:{
+
+    },
+    cards:["AH","AC","AD","AS","2H","2C","2D","2S","3H","3C","3D","3S","4H","4C","4D","4S","5H","5C","5D","5S",
+            "6H","6C","6D","6S","7H","7C","7D","7S","8H","8C","8D","8S","9H","9C","9D","9S","10H","10C","10D","10S",
+            "JH","JC","JD","JS","QH","QC","QD","QS","KH","KC","KD","KS","RJ","BJ"],
+    cardtable:{
+        deal_to: function(){
+
+        },
+        shuffle: function(){
+
+        },
+        tap_to: function(){
+
+        }
+    }
+}
+
+let player1 = checkgame.players.player1
+let player2 = checkgame.players.player2
+function deal(){
+
+}
